706bd98079abe42263363879ba6e2898bcc4486b
Tomas Nieto <81711363+nietotomas1645@users.noreply.github.com>
2022-08-16 14:07:58 -0300

    tasklist angular

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Ir a hacer las compras",
+        day: 'Agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre la programacion",
+        day: 'Agosto 5 a las 20:00',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer los apuntes",
+        day: 'Agosto 5 a las 22:00',
+        reminder: false
+    },    
+]
